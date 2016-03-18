bebec0f26343678891cf7311e05d54d62c5236fc
Mike Creel <mikecreel@gmail.com>
2016-03-18 18:44:06 -0500

    3/18/2016 Created to format dates

---
+++ goat/src/util/FormatDate.java
@@ -0,0 +1,30 @@
+package util;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author Mike
+ * Created 3/18/2016 
+ */
+public class FormatDate {
+
+	public String FormatMyDate (String MyDate) {
+		String DateToStr ="";
+		SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+		dateFormat.setLenient(false);
+		try {
+			Date date = dateFormat.parse(MyDate);
+			DateToStr = dateFormat.format(date);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return DateToStr;
+	}
+
+}
+
+
